c112953be3a353db3d450a718163417e19499953
CARLOS ALBERTO TEIXEIRA ALMEIDA <carlos.teixeira@csn.com.br>
2023-05-14 17:20:46 -0300

    Adicionando Exercicios de While

---
+++ ExercicioURI1134/ExercicioURI1134/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExercicioUsi1134
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int tipo, cont1, cont2, cont3;
+            Console.WriteLine("Digite 1.Alcool, 2.Gasolina, 3.Diesel, 4.Fim:");
+            tipo = int.Parse(Console.ReadLine());
+
+            cont1 = 0;
+            cont2 = 0;
+            cont3 = 0;
+
+            while (tipo != 4)
+            {
+                if (tipo == 1)
+                {
+                    cont1 = cont1 + 1;
+                                   }
+                else if (tipo == 2)
+                {
+                    cont2 = cont2 + 1;
+                 
+                }
+                else if (tipo == 3)
+                {
+                    cont3 = cont3 + 1;
+                   
+
+                }
+                Console.WriteLine("Digite 1.Alcool, 2.Gasolina, 3.Diesel, 4.Fim:");
+                tipo = int.Parse(Console.ReadLine());
+   
+
+            }
+            Console.WriteLine("Muito Obrigado!");
+            Console.WriteLine("Alcool: " + cont1);
+            Console.WriteLine("Gasolina: " + cont2);
+            Console.WriteLine("Diesel: " + cont3);
+        }
+    }
+}
---
+++ ExerciciosURI1117/ExerciciosURI1117/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioUri1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N1, N2, media;
+
+            Console.WriteLine("Digite a primeira nota:");
+            N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (N1 < 0 || N1 > 10)
+            {
+                Console.WriteLine("nota invalida" );
+
+                Console.WriteLine("Digite a primeira nota:");
+                N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+               
+            }
+            Console.WriteLine("Digite a segunda nota:");
+            N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (N2 < 0 || N2 > 10) 
+            {
+                Console.WriteLine("nota invalida");
+
+                Console.WriteLine("Digite a segunda nota:");
+                N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+            }
+            media = (N1 + N2) / 2;
+            Console.WriteLine("media = " + media);
+        }
+    }
+}
